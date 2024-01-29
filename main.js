76dfa6a156e07272639393724317297f9eb378d9
Marco <marcodutto1994@gmail.com>
2024-01-29 16:07:22 -0300

    se actualiza mostrarPromedio

--- main.js
+++ main.js
@@ -1,15 +1,14 @@
 const alumnos = [
-    { nombre: "Marco", nota1: 8, nota2: 9, notaFinal: 6},
-    { nombre: "Federico", nota1: 5, nota2: 2, notaFinal: 8},
-    { nombre: "Laura", nota1: 1, nota2: 4, notaFinal: 6},
-    { nombre: "Celeste", nota1: 6, nota2: 5, notaFinal: 7},
-    { nombre: "Maite", nota1: 10, nota2: 9, notaFinal: 8}
+    { nombre: "Marco", nota1: 8, nota2: 9, notaFinal: 6 },
+    { nombre: "Federico", nota1: 5, nota2: 2, notaFinal: 8 },
+    { nombre: "Laura", nota1: 1, nota2: 4, notaFinal: 6 },
+    { nombre: "Celeste", nota1: 6, nota2: 5, notaFinal: 7 },
+    { nombre: "Maite", nota1: 10, nota2: 9, notaFinal: 8 }
 ]
 
 let agregarAlumno = prompt(`Desea agregar las calificaciones de un alumno mas?`);
 
 if (agregarAlumno.toUpperCase() == "Y") {
-    console.log("hasta aca funciona");
     alumnos.push(
         {
             nombre: prompt(`Ingrese el nombre del alumno:`),
@@ -23,13 +22,15 @@ if (agregarAlumno.toUpperCase() == "Y") {
 const mostrarPromedio = (nombre) => {
     let indice = alumnos.indexOf(nombre);
 
-    if(indice != -1){
-        
-        alert(`El alumno elegido es ${alumnos(nombre)}`);
-        alert(`su promedio es: `)
-    }
+    alert(alumnos);
+
+    // if(indice != -1){
+
+    //     alert(`el alumno elegido es ${alumnos[nombre]}`)
+    //     return alumnos[nombre];
+    // }
 }
 
 // console.log(alumnos);
-let elegirAlumno = prompt(`Elegir el nombre del alumno a mostrar`);
+let elegirAlumno = prompt("Elegir el nombre del alumno a mostrar");
 mostrarPromedio(elegirAlumno);
